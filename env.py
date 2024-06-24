504aa264139559fd7f6cda474ad6d389392fe696
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-06-24 18:12:34 +0530

    .

--- env.py
+++ env.py
@@ -6,25 +6,20 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
 MUST_JOIN = os.getenv("MUST_JOIN", "")
 
 if not API_ID:
-    raise SystemExit("No API_ID found. Exiting...")
+    raise SystemExit("ɴᴏ ᴀᴘɪ_ɪᴅ ꜰᴏᴜɴᴅ. ᴇxɪᴛɪɴɢ...")
 elif not API_HASH:
-    raise SystemExit("No API_HASH found. Exiting...")
+    raise SystemExit("ɴᴏ ᴀᴘɪ_ʜᴀꜱʜ ꜰᴏᴜɴᴅ. ᴇxɪᴛɪɴɢ...")
 elif not BOT_TOKEN:
-    raise SystemExit("No BOT_TOKEN found. Exiting...")
-'''
-if not DATABASE_URL:
-    print("No DATABASE_URL found. Exiting...")
-    raise SystemExit
-'''
+    raise SystemExit("ɴᴏ ʙᴏᴛ_ᴛᴏᴋᴇɴ ꜰᴏᴜɴᴅ. ᴇxɪᴛɪɴɢ...")
 
 try:
     API_ID = int(API_ID)
 except ValueError:
-    raise SystemExit("API_ID is not a valid integer. Exiting...")
+    raise SystemExit("ᴀᴘɪ_ɪᴅ ɪꜱ ɴᴏᴛ ᴀ ᴠᴀʟɪᴅ ɪɴᴛᴇɢᴇʀ. ᴇxɪᴛɪɴɢ...")
 
 if 'postgres' in DATABASE_URL and 'postgresql' not in DATABASE_URL:
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
